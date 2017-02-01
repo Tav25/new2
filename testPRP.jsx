1142a8140cb6410b1bbed03694bfd00937a5f538
Tav25 <turbin28@gmail.com>
2017-02-01 15:32:34 +0300

    1

---
+++ testPRP.jsx
@@ -0,0 +1,13 @@
+﻿"#target premierepro";
+alert(app.project.name);
+alert (app.project.rootItem.children.numItems)
+//app.project.createNewSequence('555', '001','65455');
+var activeSequence = app.project.activeSequence;
+alert(activeSequence.name)
+
+var arr = []
+arr[0] = "d:/1.mp4"
+arr[1] = "d:/1a.mp4"
+app.project.importFiles(arr);
+
+alert (app.project.rootItem.children[1])
