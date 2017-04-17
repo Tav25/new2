078d7467cd1c9dd171d52f23fdab2f412a1e25c2
Tav25 <turbin28@gmail.com>
2017-04-17 14:52:56 +0300

    17.04

---
+++ egg.jsx
@@ -0,0 +1,10 @@
+﻿//alert('555')
+//alert(app.project.item(2).name)
+
+app.project.renderQueue.items.add(app.project.item(2));
+app.project.renderQueue.items.add(app.project.item(3));
+app.project.renderQueue.items.add(app.project.item(4));
+app.project.renderQueue.items.add(app.project.item(5));
+app.project.renderQueue.items.add(app.project.item(6));
+app.project.renderQueue.items.add(app.project.item(7));
+
