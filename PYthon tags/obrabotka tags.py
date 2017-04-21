8e2fe0de3eeb6ef6e356ebea75cd62793629ef32
Tav25 <turbin28@gmail.com>
2017-04-21 22:16:46 +0300

    z

--- PYthon tags/obrabotka tags.py
+++ PYthon tags/obrabotka tags.py
@@ -1,45 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-fil='spisES.txt'
+fil='obrabotka tags.txt'
 i=0
 x=[]
 y=[]
 for line in open(fil):
         f=line
         if not f in x:
-                
-                #y.append(f)
-                #print('x:_'+str(x))
-                #print('y:_'+str(y))
-                #d=input()
-                #sl='dctc'+'\n'
                 sl=f
                 i2=0
                 for line in open(fil):
                         if sl==line:
                                 i2=i2+1
                                 #print(i2)
-                if i2>50:
+                if i2>50:#
                         x.append(f)
                         y.append(i2)
         i=i+1
-	#print(line)
-#print(i)
-#print(len(x))
-#print(len(y))
-#print(x)
-#print(y)
-#print(max(y))
-
 print(y.index(max(y)))#slovo
 print(x[y.index(max(y))])#kolichestvo
 xy=[]
 i3=0
+f = open('vuhod.txt','w')
 while len(x)>i3:
 	i3+=1
 	#print(y.index(max(y)))
 	xy.append(str(max(y))+x[y.index(max(y))])
 	y[y.index(max(y))]=0
-
-
+	#f.write(x[y.index(max(y))][0:-2]+', ')
+	f.write(x[y.index(max(y))])
+f.close()
 input()
+
