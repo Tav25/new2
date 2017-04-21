5736eabccb41f06dda7dc54493f65142a1ee2a18
Tav25 <turbin28@gmail.com>
2017-04-21 15:44:43 +0300

    z

--- PYthon tags/obrabotka tags.py
+++ PYthon tags/obrabotka tags.py
@@ -7,28 +7,39 @@ y=[]
 for line in open(fil):
         f=line
         if not f in x:
-                x.append(f)
+                
                 #y.append(f)
                 #print('x:_'+str(x))
                 #print('y:_'+str(y))
                 #d=input()
-                sl='Disney Baby Toys'+'\n'
+                #sl='dctc'+'\n'
+                sl=f
                 i2=0
                 for line in open(fil):
                         if sl==line:
                                 i2=i2+1
-                                print('!!!')
-                y.append(f)
+                                #print(i2)
+                if i2>50:
+                        x.append(f)
+                        y.append(i2)
         i=i+1
 	#print(line)
 #print(i)
-print(len(x))
-print(len(y))
-#print(f)
+#print(len(x))
+#print(len(y))
+#print(x)
+#print(y)
+#print(max(y))
+
+print(y.index(max(y)))#slovo
+print(x[y.index(max(y))])#kolichestvo
+xy=[]
+i3=0
+while len(x)>i3:
+	i3+=1
+	#print(y.index(max(y)))
+	xy.append(str(max(y))+x[y.index(max(y))])
+	y[y.index(max(y))]=0
 
 
-sl='Disney Baby Toys'+'\n'
-for line in open(fil):
-        if sl==line:
-                print('!!!')
 input()
